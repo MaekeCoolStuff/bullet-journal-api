39432f50d521b36410aa5a380c6cb5de5fc4fab5
Mae van Dijk <mae.vandijk1990@gmail.com>
2019-12-23 16:38:49 +0100

    azure werkend

--- BulletJournalApi/Startup.cs
+++ BulletJournalApi/Startup.cs
@@ -88,6 +88,7 @@ namespace BulletJournalApi
                 }
                 );
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
